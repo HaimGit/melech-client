57ec1a7a10db1e73555a8ef2b8e0943eb7c25b12
haimtheone1@gmail.com <haimtheone1@gmail.com>
2026-03-01 12:24:02 +0200

    Melech client initialization commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'melech',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'melech-client';
+}
