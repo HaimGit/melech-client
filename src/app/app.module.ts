9fa5191b5686492aae81193402068cc0ca66698e
noam <nuna030506gmail.com>
2026-03-29 16:29:30 +0300

    feat: added panel compoent & icon folder in assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PanelComponent } from './components/panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/panel/panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'panels',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.less']
+})
+export class PanelComponent {
+
+}
