02fbd7749fb5763bcced9842edb70dd5bba23806
noam <nuna030506gmail.com>
2026-03-31 09:36:22 +0300

    fix: post pr fixes

--- src/app/components/panel/weeklyTablePanel.component.ts
+++ src/app/components/panel/weeklyTablePanel.component.ts
@@ -3,14 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'weekly-table-panel',
   templateUrl: './weeklyTablePanel.component.html',
-  styleUrls: ['./weeklyTablePanel.component.less']
+  styleUrls: ['./weeklyTablePanel.component.less'],
 })
-export class WeeklyTablePanelsComponent  {
-
-specialEvents = [{name: "תרגיל אנשי צוות", count: "15/30"}, {name: "תרגיל אנשי צוות", count: "17/30"}];
-missingPositions = [{day: "'יום ד", count: 1}, {day: "'יום ו", count: 2}];
-missingPositionsSum = 3;
-requiredTeam = {reservistCount: 3, regularsCount: 2, sum: 5};
-
+export class WeeklyTablePanelsComponent {
+  specialEvents = [
+    { name: 'תרגיל אנשי צוות', count: '15/30' },
+    { name: 'תרגיל אנשי צוות', count: '17/30' },
+  ];
+  missingPositions = [
+    { day: "'יום ד", count: 1 },
+    { day: "'יום ו", count: 2 },
+  ];
+  missingPositionsSum = 3;
+  requiredTeam = { reservistCount: 3, regularsCount: 2, sum: 5 };
 }
-
